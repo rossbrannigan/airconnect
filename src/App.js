3dd9f4d0dc30cddc9500b3b6af5258af642bc21a
Ross Brannigan <rossbrannigan@gmail.com>
2024-03-22 10:08:45 +0700

    GA React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import airconnectImage from './images/airconnect.svg'; // Path to the image
+import ReactGA from 'react-ga';
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-0PBTF2YNYG');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
